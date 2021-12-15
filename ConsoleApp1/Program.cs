23b88572622f04a4d429fabe11066247cce95b98
Ivan Srsen <Ivan.Srsen@ysoft.com>
2021-12-15 11:10:43 +0100

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var discoveryDocument = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (discoveryDocument.IsError)
+            {
+                Console.WriteLine(discoveryDocument.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "TestApi"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.ReadLine();
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            Console.WriteLine("IDENTITY TOKEN----------" + tokenResponse.IdentityToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44308/identity");
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IdentityServerPlayground/Config.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerPlayground
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("TestApi", "Test API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "TestApi" }
+                }
+            };
+    }
+}
